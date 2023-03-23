35045e097983ddfc4fe29f70af086439852358df
Farooq Karimi Zadeh <fkz@riseup.net>
2023-03-23 19:43:36 +0330

    InIt

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Component } from "inferno";
+
+export default class App extends Component {
+  render() {
+    return (
+      <p>
+        Hello World! This has to be filled!
+      </p>
+    );
+  }
+};
